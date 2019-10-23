96ffecbab6b2447fb0dcf8996e9b071b939db21c
mikikumi240 <mikikumi240@gmail.com>
2019-10-23 10:55:52 +0300

    design changes in registration compo

--- src/NewRegistration.js
+++ src/NewRegistration.js
@@ -100,53 +100,53 @@ class NewRegistration extends React.Component  {
       }
       else{
        return (
-         <div style={{width:"50vw",border:"1px solid orange"}}>
-         <h3 className="mb-5">{this.props.target==="LOGGING"?"מסך כניסה":"מסך רישום"}</h3>
-         <Form>
-           <Form.Group as={Row} controlId="formUN">
-             <Form.Label column sm="2">
+         <div style={{width:"25vw",border:"1px solid black"}}>
+         <h3 className="mt-3 mx-auto w-50 text-center">{this.props.target==="LOGGING"?"מסך כניסה":"מסך רישום"}</h3>
+         <Form className="mt-5" style={{width:"25vw",border:"0px solid red"}}>
+           <Form.Group as={Row} controlId="formUN" className="mx-auto">
+             <Form.Label column sm="3">
                שם משתמש
              </Form.Label>
-             <Col sm="4">
+             <Col sm="5">
                <Form.Control type="text" onChange={this.readInputValue} data-id="txtUN"  />
              </Col>
            </Form.Group>
      
-           <Form.Group as={Row} controlId="formPassword">
-             <Form.Label  column sm="2">
+           <Form.Group as={Row} controlId="formPassword" className="p-3">
+             <Form.Label  column sm="3">
                סיסמא
              </Form.Label>
-             <Col sm="4">
+             <Col sm="5">
                <Form.Control type="password" onChange={this.readInputValue} data-id="txtPsw"  />
              </Col>
            </Form.Group>
             
-            <Form.Group as={Row} controlId="formNickName" className={this.props.target==="LOGGING"?"hide":""}>
-             <Form.Label column sm="2">
+            <Form.Group as={Row} controlId="formNickName" className= {this.props.target==="LOGGING"?"hide":"" +" p-3"}>
+             <Form.Label column sm="3">
                שם לתצוגה
              </Form.Label>
-             <Col sm="4">
+             <Col sm="5">
                <Form.Control type="text" onChange={this.readInputValue} data-id="txtDisplayName"  />
              </Col>
             </Form.Group>
   
   
             
-           <Form.Group as={Row} controlId="formAdmin" className={this.props.target==="LOGGING"?"hide":""} >
+           <Form.Group as={Row} controlId="formAdmin" className={ this.props.target==="LOGGING"?"hide":"" +" p-3"} >
             
-            <Col sm="2">
+            <Col sm="3">
              <Form.Check inline label="&nbsp;מנהל" type="checkbox" onChange={this.readInputValue} data-id="chkAdmin"/>
             </Col>
-             <Col sm="4">
+             <Col sm="5">
                <Form.Control type="password" onChange={this.readInputValue} data-id="txtAdmin" className={this.state.isAdmin==false?"hide":"display"} />
              </Col>
            </Form.Group>
-           <Form.Group as={Row} className="justify-content-start" style={{}} controlId="formAdmin">
-           <Col sm="4"><Button variant="primary" type="button" onClick={this.okPressed}>
-             אישור
-             </Button>
+           <Form.Group as={Row} className="justify-content-center w-50 " style={{border:"0px solid"}} controlId="formAdmin">
+             <Col sm="4"><Button variant="primary" type="button" onClick={this.okPressed}>
+              אישור
+              </Button>
              </Col>
-             <Col sm="4"><Button variant="primary" type="button" href="/">
+             <Col sm="4"><Button variant="secondary" type="button" href="/">
              ביטול
              </Button></Col>
            </Form.Group>         
